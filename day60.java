19d512762869988ad1cbdf2feab01751c2ea0c10
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-02-14 22:58:09 +0530

    Implement method to check if a number has three divisors

---
+++ day60.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isThree(int n) {
+        int div = 0;
+        for(int i = 1; i<=n; i++){
+            if(n%i==0){
+                div++;
+            }
+        }
+        return (div==3)? true: false;
+    }
+}
