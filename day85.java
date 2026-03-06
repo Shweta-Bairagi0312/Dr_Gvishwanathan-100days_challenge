6ed34751da155a8a769c09a329ae0b789de7d28f
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-03-06 23:08:11 +0530

    Implement findAndReplacePattern method in Solution class

---
+++ day85.java
@@ -0,0 +1,8 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> res = new ArrayList<>();
+        for (String word : words) {
+            if (check(word, pattern)) res.add(word);
+        }
+        return res;
+    }
